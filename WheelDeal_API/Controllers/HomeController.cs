01942873e1568f4f861b89341cdbdfa01c1b74c5
SyedFahad110 <127405710+Syedfahad039@users.noreply.github.com>
2025-06-02 01:39:48 +0500

    Make Db Context Class

---
+++ WheelDeal_API/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WheelDeal_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
---
+++ WheelDeal_API/DbContexts/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WheelDeal_API.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
